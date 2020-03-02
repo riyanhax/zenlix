a3bb76ee5fc1ccefc155cd6815390ca0641ad4cb
aravael <arava888@gmail.com>
2020-03-02 14:08:48 +0200

    dev

--- app/controllers/list_content.inc.php
+++ app/controllers/list_content.inc.php
@@ -100,9 +100,9 @@ if (isset($_POST['menu'])) {
             try {
                 $noRules = true;
                 $stmt = $dbConnection->prepare(
-                    "SELECT ticket_id FROM ticket_log WHERE (UNIX_TIMESTAMP(date_op) + 86400) > UNIX_TIMESTAMP(NOW()) AND init_user_id IN (:uids) GROUP BY ticket_id"
+                    "SELECT ticket_id FROM ticket_log WHERE (UNIX_TIMESTAMP(date_op) + 86400) > UNIX_TIMESTAMP(NOW()) AND init_user_id IN ($collegues) GROUP BY ticket_id"
                 );
-                $stmt->execute([':uids' => $collegues]);
+                $stmt->execute();
                 $idts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 $idts = f3pick($idts,'ticket_id');
